895c815c426172b2f84bb21db6ebb6246f0095ea
gmartineza <gabymartinezalarcon@gmail.com>
2023-09-11 00:43:07 -0300

    add all rotateRight tests

--- src/test/java/PieceBaseTest.java
+++ src/test/java/PieceBaseTest.java
@@ -34,105 +34,12 @@ public class PieceBaseTest{
         assertNotNull(t);
     }
 
-    // TODO: verificar piezas
-   /* 
-    @Test
-    public void check_DogPiece_true() {
-
-        DogPieceRight T1 = new DogPieceRight();
-
-        assertEquals(true, T1.checkPiece("1100110000000000"));
-
-    }
-    @Test
-    public void ccheck_DogPiece_false() {
-
-       DogPieceRight T1 = new DogPieceRight();
-
-        assertEquals(false, T1.checkPiece("0100111000000000"));
-
-    }
-
-    @Test
-    public void check_LPiece_true() {
-
-        LPieceRight T1 = new LPieceRight();
-
-            assertEquals(true, T1.checkPiece("1000100010001100"));
-
-    }
-
-    @Test
-        public void check_LPiece_false() {
-
-        LPieceRight T1 = new LPieceRight();
-
-            assertEquals(false, T1.checkPiece("10100100010001100"));
-
-    }
-
-    @Test
-    public void check_SquarePiece_true() {
-
-            SquarePiece T1 = new SquarePiece();
-
-                assertEquals(true, T1.checkPiece("1100110000000000"));
-
-    }
-
-    @Test
-       public void check_SquarePiece_false() {
-
-            SquarePiece T1 = new SquarePiece();
-
-            assertEquals(false, T1.checkPiece("11100100010001100"));
-
-    }
-
-    @Test
-        public void check_StickPiece_true() {
-
-            StickPiece T1 = new StickPiece();
-
-                assertEquals(true, T1.checkPiece("1000100010001000"));
-
-    }
-
-    @Test
-       public void check_StickPiece_false() {
-
-           StickPiece T1 = new StickPiece();
-
-            assertEquals(false, T1.checkPiece("11100100010001100"));
-
-    }
-
-     @Test
-    public void check_TPiece_true() {
-
-        TPiece T1 = new TPiece();
-
-        assertEquals(true, T1.checkPiece("0100111000000000"));
-
-    }
-
-    @Test
-    public void check_TPiece_false() {
-
-        TPiece T1 = new TPiece();
-
-        assertEquals(false, T1.checkPiece("1000100010001000"));
-
-    }
-
-*/
-
     @Test
     public void rotateClowkwiseDog() {
 
         DogPieceLeft dogL = new DogPieceLeft();
 
-       byte[][] dogExpected =  new byte[][] {
+        byte[][] dogExpected = new byte[][] {
         { 0, 1 },
         { 1, 1 },
         { 1, 0 }};
@@ -142,9 +49,52 @@ public class PieceBaseTest{
        assertArrayEquals(dogExpected,dogL.getCurrentOrientation());
     }
     
+    
     @Test
-    public void testOrientations() {
-        LPieceLeft L1 = new LPieceLeft();
+    public void testOrientationsDogPieceLeft() {
+        DogPieceLeft testPiece = new DogPieceLeft();
+        
+         byte[][][] expectedOrientations = {{
+                { 1, 1, 0 },
+                { 0, 1, 1 }
+            },
+            {
+                { 0, 1 },
+                { 1, 1 },
+                { 1, 0 }
+            }
+        };
+
+        for (int i = 0; i < expectedOrientations.length; i++) {
+            assertArrayEquals(expectedOrientations[i], testPiece.getCurrentOrientation());
+            testPiece.rotateRight();
+        }
+    }
+
+   @Test
+   public void testOrientationsDogPieceRight() {
+       DogPieceRight testPiece = new DogPieceRight();
+       
+         byte[][][] expectedOrientations = {{
+                { 0, 1, 1 },
+                { 1, 1, 0 }
+            },
+            {
+                { 1, 0 },
+                { 1, 1 },
+                { 0, 1 }
+            }
+        };
+
+        for (int i = 0; i < expectedOrientations.length; i++) {
+            assertArrayEquals(expectedOrientations[i], testPiece.getCurrentOrientation());
+            testPiece.rotateRight();
+        }
+    }
+
+    @Test
+    public void testOrientationsLPieceLeft() {
+        LPieceLeft testPiece = new LPieceLeft();
 
         // Definir las orientaciones esperadas en el mismo orden que se definen en LPieceLeft
          byte[][][] expectedOrientations = {
@@ -153,48 +103,122 @@ public class PieceBaseTest{
                 { 1, 1, 1 }
             },
             {
+                { 0, 1 },
+                { 0, 1 },
+                { 1, 1 }
+            },
+            {
+                { 1, 1, 1 },
+                { 0, 0, 1 }
+            },
+            {
                 { 1, 1 },
                 { 1, 0 },
                 { 1, 0 }
+            }
+        };
+
+        // Probar todas las orientaciones
+        for (int i = 0; i < expectedOrientations.length; i++) {
+            assertArrayEquals(expectedOrientations[i], testPiece.getCurrentOrientation());
+            testPiece.rotateRight();
+        }
+    }
+    
+    @Test
+    public void testOrientationsLPieceRight() {
+        LPieceRight testPiece = new LPieceRight();
+
+         byte[][][] expectedOrientations = {{
+                { 0, 0, 1 },
+                { 1, 1, 1 }
+            },
+            {
+                { 1, 1 },
+                { 0, 1 },
+                { 0, 1 }
             },
             {
                 { 1, 1, 1 },
                 { 1, 0, 0 }
             },
             {
+                { 1, 0 },
+                { 1, 0 },
+                { 1, 1 }
+            }
+        };
+
+        for (int i = 0; i < expectedOrientations.length; i++) {
+            assertArrayEquals(expectedOrientations[i], testPiece.getCurrentOrientation());
+            testPiece.rotateRight();
+        }
+    }
+
+    @Test
+    public void testOrientationsSquarePiece() {
+        SquarePiece testPiece = new SquarePiece();
+
+         byte[][][] expectedOrientations = {{
+            {1, 1},
+            {1, 1}
+        }};
+
+        for (int i = 0; i < expectedOrientations.length; i++) {
+            assertArrayEquals(expectedOrientations[i], testPiece.getCurrentOrientation());
+            testPiece.rotateRight();
+        }
+    }
+
+    @Test
+    public void testOrientationsStickPiece() {
+        StickPiece testPiece = new StickPiece();
+
+         byte[][][] expectedOrientations = {{
+                { 1 },
+                { 1 },
+                { 1 },
+                { 1 }
+            },
+            {
+                { 1, 1, 1, 1 }
+            }
+        };
+
+        for (int i = 0; i < expectedOrientations.length; i++) {
+            assertArrayEquals(expectedOrientations[i], testPiece.getCurrentOrientation());
+            testPiece.rotateRight();
+        }
+    }
+
+    @Test
+    public void testOrientationsTPiece() {
+        TPiece testPiece = new TPiece();
+
+         byte[][][] expectedOrientations = {
+            {
+                { 0, 1, 0 },
+                { 1, 1, 1 }
+            },
+            {
+                { 1, 0 },
                 { 1, 1 },
+                { 1, 0 }
+            },
+            {
+                { 1, 1, 1 },
+                { 0, 1, 0 }
+            },
+            {
                 { 0, 1 },
+                { 1, 1 },
                 { 0, 1 }
             }
         };
 
-        // Probar todas las orientaciones
         for (int i = 0; i < expectedOrientations.length; i++) {
-            assertArrayEquals(expectedOrientations[i], L1.getCurrentOrientation());
-            L1.rotateRight();
+            assertArrayEquals(expectedOrientations[i], testPiece.getCurrentOrientation());
+            testPiece.rotateRight();
         }
     }
-
-
-
-
-    /* 
-    @Test
-    public void rotate_full_clockwise(){
-        //Piece t = new DogPiece();
-        //Piece square = new DogPiece();
-        //Piece stick = new DogPiece();
-        //Piece l = new DogPiece();
-        Piece dog = new DogPiece();
-    }
-     
-      // llamar funcion que rote (rotate(counter/clockwise))y chequear que la rota bien (getPieceConfiguration())
-        for (short i = 0; i < 4; i++) {
-            dog.rotate(clockwise);
-            assertEquals(dog.expectedPieceConfiguration(i), dog.getPieceConfiguration());
-            // TODO: implementar expectedPieceConfiguration donde cada nro corresponde a una rotacion
-        }
-    
-    }
-    */
 }
