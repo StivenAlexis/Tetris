a8927e9e65b63059eb66551b89de72900c01c859
Stiven Alexis <stiven22monsalvo@gmail.com>
2023-09-12 11:29:18 -0300

    upadate tetris.start()

--- src/main/java/gmartineza/tetris/Tetris.java
+++ src/main/java/gmartineza/tetris/Tetris.java
@@ -1,63 +1,57 @@
 package gmartineza.tetris;
+import java.time.Clock;
+import java.util.Random;
 
 public class Tetris {
     private Board board;
     private PieceBase currentPiece;
-
-    public void start() {
+    private Clock clock;
+    private Random random = new Random();
+    int row = 0;
+    int randomCol;
+    int col = 0;
         // Iniciar el juego
-        while (true) {
-           
-            // Insertar una nueva pieza aleatoria en la parte superior
-            currentPiece = board.spawnNewPiece();
-    
-            // Bajar la pieza hasta llegar al final
-            int row=0;
-            int col = 0;
-            Object ol;
-            while (canPlacePiece(currentPiece,row,col)) {
-                movePieceDown();
-            }
-    
-            // Rotar la pieza
-            rotatePiece();
-    
-            // Continuar bajando la pieza hasta llegar al final
-            while (canPlacePieceAt(row + 1, randomCol)) {
-                movePieceDown();
-            }
-    
-            // Comprobar si el juego ha terminado (por ejemplo, si no se puede insertar una nueva pieza)
-            if (!canInsertNewPiece()) {
-                break; // El juego ha terminado
+
+        public void start() {
+            
+        
+            while (true) {
+                // Insertar una nueva pieza aleatoria en la parte superior
+                currentPiece = board.spawnNewPiece();
+                System.out.println();
+                // Bajar la pieza hasta llegar al final
+            
+                 while (board.canPlacePiece(currentPiece, row, col)) {
+            
+                   board.movePiece(currentPiece, row + 1);
+                   
+                }
+        
+                // Rotar la pieza
+                currentPiece.rotateLeft();
+        
+                // Continuar bajando la pieza hasta llegar al final
+               while (board.canPlacePiece(currentPiece, row, col)) {
+                   board.movePiece(currentPiece, row + 1);
+                   
+                }
+                
+                for (row = 0; row < 21; row++) {
+                    board.isSingleLine(row);
+                }
+
+                
+                
+                
+                // Comprobar si el juego ha terminado ( Si no se puede insertar una nueva pieza o hacer 5 tetris)
+                if (! board.canPlacePiece(currentPiece,0,0)||  ) {
+                    break; // El juego ha terminado
+                }
             }
         }
-    }
+        
     
-    private boolean canPlacePiece(PieceBase currentPiece2, int row, int col) {
-        return false;
-    }
 
-    private boolean canPlacePieceAt(int row, int col) {
-        // Comprobar si la pieza actual se puede colocar en la posición especificada utilizando la función de Board
-        return board.canPlacePiece(currentPiece, row, col);
-    }
-    
-    private void movePieceDown() {
-        // Mover la pieza actual hacia abajo utilizando la función de Board
-        board.movePiece(currentPiece, row + 1, randomCol, true);
-        row++;
-    }
-    
-    private void rotatePiece() {
-        // Rotar la pieza actual utilizando la función de Board
-        board.rotatePiece(currentPiece);
-    }
-    
-    private boolean canInsertNewPiece() {
-        // Comprobar si se puede insertar una nueva pieza en la parte superior del tablero utilizando la función de Board
-        return board.canInsertNewPiece();
-    }
     
 
 
