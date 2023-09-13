f84d7da6866cc6b5eb2012ab704210482bf9ca89
gmartineza <gabymartinezalarcon@gmail.com>
2023-09-13 08:22:17 -0300

    modify tetris.start() to prevent infinite loop, add tetris tests

--- src/main/java/gmartineza/tetris/Tetris.java
+++ src/main/java/gmartineza/tetris/Tetris.java
@@ -1,12 +1,11 @@
 package gmartineza.tetris;
 
-
 public class Tetris {
     private Board board;
     private PieceBase currentPiece;
     private Clock clock;
-    private int Count=0;
-    private int status = 0 ; 
+    private int count = 0;
+    private int status = 0;
     private String mensajeStatus = "";
     int row = 0;
     int randomCol;
@@ -16,85 +15,53 @@ public class Tetris {
         this.clock = clock;
         this.board = board;
     }
-        // Iniciar el juego
-    
-        public void start() {
-            
-        status = 1;
-            while (status == 1) {
-                // Insertar una nueva pieza aleatoria en la parte superior
-                currentPiece = board.spawnNewPiece();
-                
-                // Bajar la pieza hasta llegar al final
-                tick();
-                
-                
-                Count = board.lineCount();
+    // Iniciar el juego
+    public void start() {
+    status = 1;
 
-                // Comprobar si el juego ha terminado ( Si no se puede insertar una nueva pieza o hacer 5 tetris)
-                if (! board.getCanPlacePiece(currentPiece,0,0) ) {
-                    status = 2; // El juego ha terminado
-                } else if( Count==5 ){
-                    status = 3;
-                }
-            }
-        }
-        
-        public String state(){
+    // Insertar una nueva pieza aleatoria en la parte superior
+    currentPiece = board.spawnNewPiece();
 
-            if(status == 0){
+    // Bajar la pieza hasta llegar al final
+    tick();
 
-                mensajeStatus = "No iniciado";
+    count = board.lineCount();
 
-            }else if(status == 1){
-
-                mensajeStatus = "Jugando";
-
-            }else if(status ==2 ){
-
-                    mensajeStatus = "Game Over";
-
-            }else mensajeStatus = "You are Winner"; 
-
-        return mensajeStatus;    
-        }
-
-        public void tick(){
-
-         while (board.getCanPlacePiece(currentPiece, row, col)) {
-            
-               board.movePiece(currentPiece, row + 1);
-                
-               clock.tick();
-            }
-        }
-        
-
-        public void  rotateLeft(){
-
-            currentPiece.rotateLeft();
-        }
-        
-        public void rotateRight(){
-
-            currentPiece.rotateRight();
-        }
+    // Comprobar si el juego ha terminado ( Si no se puede insertar una nueva pieza
+    // o hacer 5 tetris)
+    if (!board.getCanPlacePiece(currentPiece, 0, 0)) {
+        status = 2; // El juego ha terminado
+    } else if (count == 5) {
+        status = 3;
+    }
 }
 
-         
-         
 
+    public String state() {
+        if (status == 0) {
+            mensajeStatus = "No iniciado";
+        } else if (status == 1) {
+            mensajeStatus = "Jugando";
+        } else if (status == 2) {
+            mensajeStatus = "Game Over";
+        } else
+            mensajeStatus = "You are Winner";
 
+        return mensajeStatus;
+    }
 
+    public void tick() {
+        while (board.getCanPlacePiece(currentPiece, row, col)) {
+            board.movePiece(currentPiece, row + 1);
+            clock.tick();
+        }
+    }
 
+    public void rotateLeft() {
+        currentPiece.rotateLeft();
+    }
 
-        
-    
-
-    
-
-
-
-
-
-
+    public void rotateRight() {
+        currentPiece.rotateRight();
+    }
+}
--- src/test/java/TetrisTest.java
+++ src/test/java/TetrisTest.java
@@ -3,6 +3,35 @@ import static org.junit.Assert.*;
 import gmartineza.tetris.*;
 
 public class TetrisTest {
+    @Test
+    public void IntantiateTetrisTest(){
+        Board b = new Board();
+        Clock c = new Clock();
+        Tetris t = new Tetris(b, c);
+
+        assertNotNull(b);
+        assertNotNull(c);
+        assertNotNull(t);
+    }
     
-    
+    @Test
+    public void tetrisState0Test(){
+        Board b = new Board();
+        Clock c = new Clock();
+        Tetris t = new Tetris(b, c);
+
+        assertEquals("No iniciado",t.state());
+    }
+
+
+
+    @Test
+    public void tetrisState1Test(){
+        Board b = new Board();
+        Clock c = new Clock();
+        Tetris t = new Tetris(b, c);
+        t.start();
+
+        assertEquals("Jugando",t.state());
+    }
 }
